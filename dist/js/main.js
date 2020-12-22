e98dfdfece4782a7c81ffc87e179a50a4cc88215
Tarun <tarun.karmakar.dev@gmail.com>
2020-12-22 21:00:54 +0530

    Added contact form and footer

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,41 @@
+
 let menuButton = $('.menu-button');
 let navbar = $('nav')
 // console.log(menuButton);
 
 menuButton.click(function () {
     navbar.toggleClass('open');
+
+    if ($('body').css("overflow") == "hidden") {
+        $('body').css("overflow", "auto");
+    }
+    else {
+        $('body').css("overflow", "hidden");
+    }
+
+});
+
+
+let formInput = $('.form-row input, .form-row textarea');
+
+formInput.focus(function () {
+
+    if ($(this).attr("type") != "submit") {
+        $(this).parents('.form-row').toggleClass('focused');
+        if ($(this).is("textarea")) {
+            $(this).attr("rows", "10");
+            $(this).prev("label").css("transform", "translateY(-90px)");
+        }
+    }
+});
+
+
+formInput.blur(function () {
+    if ($(this).attr("type") != "submit") {
+        $(this).parents('.form-row').toggleClass('focused');
+        if ($(this).is("textarea")) {
+            $(this).attr("rows", "10");
+            $(this).prev("label").css("transform", "translateY(0)");
+        }
+    }
 });
